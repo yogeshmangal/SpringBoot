c76cd5a666af5a55162d43f74208bc5125aaeffd
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2024-02-01 23:45:34 +0530

    java-config-bean

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/SwimCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springcoredemo.common;
+
+public class SwimCoach implements Coach{
+    public SwimCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Swim Coach";
+    }
+}
---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+    @Autowired
+    public DemoController(@Qualifier("swimCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello yogesh!!";
+    }
+
+    @GetMapping("/dailyworkout")
+    public String daily() {
+        return myCoach.getDailyWorkout();
+    }
+}
