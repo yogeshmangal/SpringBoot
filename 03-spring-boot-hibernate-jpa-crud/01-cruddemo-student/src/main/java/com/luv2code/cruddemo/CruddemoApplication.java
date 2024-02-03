8bb087f757baaa46e6a03497043a0b5633d8a3e2
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2024-02-03 23:04:05 +0530

    Hibernate-JPA
    CommandLineRunner application

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,21 @@
+package com.luv2code.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return shalu -> {
+			System.out.println("Hello World");
+		};
+	}
+}
