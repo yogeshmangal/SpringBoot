ff1d9d49d5f16dea708f7bcf258bba743eede30d
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2024-02-14 23:18:45 +0530

    PathVariable Annotation

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> studentList;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> studentList = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        studentList = new ArrayList<>();
         studentList.add(new Student("Yogesh", "Mangal"));
         studentList.add(new Student("Aman", "Gupta"));
         studentList.add(new Student("Sagar", "Gulati"));
         studentList.add(new Student("Maneesh", "Agrahari"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return studentList;
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentByID(@PathVariable int id) {
+        return studentList.get(id);
     }
 }
