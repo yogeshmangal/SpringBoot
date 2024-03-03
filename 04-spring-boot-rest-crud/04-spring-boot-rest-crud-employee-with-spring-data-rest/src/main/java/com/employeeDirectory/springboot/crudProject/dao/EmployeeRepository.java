1b22fa438964d023562fea04e87cca37338b7fa4
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2024-03-03 22:40:40 +0530

    API Design: REST API For EMPLOYEE using Spring Data REST.

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/employeeDirectory/springboot/crudProject/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.employeeDirectory.springboot.crudProject.dao;
+
+import com.employeeDirectory.springboot.crudProject.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
