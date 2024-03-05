80691933ae59dd20a158591bece246486bf23d0e
Yogesh Mangal <67264564+yogeshmangal@users.noreply.github.com>
2024-03-05 22:59:26 +0530

    Configuration, Pagination and Sorting.

---
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest -configs/src/main/java/com/employeeDirectory/springboot/crudProject/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employeeDirectory.springboot.crudProject.dao;
+
+import com.employeeDirectory.springboot.crudProject.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
